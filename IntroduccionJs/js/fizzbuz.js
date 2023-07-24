f5803faf8ff343621b29de1217dbc92f6eea2bb8
Nahum Martinez <111940941+NahumMartinez01@users.noreply.github.com>
2023-07-24 09:39:31 -0600

    añadimos carpeta piedra papel y tijera

---
+++ IntroduccionJs/js/fizzbuz.js
@@ -0,0 +1,15 @@
+const numeros = parseInt(prompt("Ingrese una cantidad de repeticiones a realizar") )
+
+
+for(let i  = 1; i <= numeros; i++){
+
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log( "fizzbuzz")
+    } else if(i % 5 === 0){
+        console.log( "buzz")
+    }else   if(i % 3 === 0){
+        console.log( "fizz")
+    }else {
+        console.log(i)
+    }
+}
